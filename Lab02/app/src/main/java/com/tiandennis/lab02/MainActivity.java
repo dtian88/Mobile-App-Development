03989bb78a09530eec28d49a219fbcbed3f49365
dtian88 <dtian88@gmail.com>
2020-02-13 09:37:54 -0500

    Fixed Lab01 string splitting error; created lab 2

--- Lab02/app/src/main/java/com/tiandennis/lab02/MainActivity.java
+++ Lab02/app/src/main/java/com/tiandennis/lab02/MainActivity.java
@@ -1,4 +1,4 @@
-package com.tiandennis.lab01;
+package com.tiandennis.lab02;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,8 +11,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     Button submitButton, colorButton;
     EditText responseText;
-    TextView displayText;
-    int number = 1;
+    TextView displayText, arrayText;
+    String[] array;
+    long number = 1;
+    int index = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         colorButton=findViewById(R.id.colorButton);
         responseText=findViewById(R.id.responseEditText);
         displayText=findViewById(R.id.textBox);
+        arrayText=findViewById(R.id.arrayBox);
+        array=getResources().getStringArray(R.array.array);
         displayText.setText(displayText.getText()+" Number: " + number + ". ");
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("submitButton","Logcat: "+responseText.getText().toString());
                 String old = displayText.getText().toString();
                 if(old.contains("!"))
-                     displayText.setText(old.split(", ")[0] + ", " + responseText.getText()+"!");
+                    displayText.setText(old.split(", ")[0] + ", " + responseText.getText()+"!");
                 else
                     displayText.setText(displayText.getText() + "Thanks for using this quiz app, " + responseText.getText()+"!");
             }
@@ -43,7 +47,13 @@ public class MainActivity extends AppCompatActivity {
                 submitButton.setBackgroundColor(color | (int)(Math.random() * (0xFFFFFF + 2)));
                 String[] old1 = displayText.getText().toString().split(": ");
                 number *= 2;
+                if (number == 0)
+                    number = 1;
                 displayText.setText(old1[0] + ": " + number + "." + old1[1].split("\\.")[1]);
+                arrayText.setText(array[index]);
+                index++;
+                if(index == array.length)
+                    index = 0;
             }
         });
         responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
