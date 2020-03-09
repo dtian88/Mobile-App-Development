e35c0736699f5805b509df4727cc6d45c3472efd
dtian88 <dtian88@gmail.com>
2020-03-09 17:25:47 -0400

    Made GUI, game logic, user input, reset, and conflict checking. A fully functional game now.

--- Sudoku/app/src/main/java/com/tiandennis/sudoku/MainActivity.java
+++ Sudoku/app/src/main/java/com/tiandennis/sudoku/MainActivity.java
@@ -1,13 +1,33 @@
 package com.tiandennis.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button reset;
+    DrawView d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new DrawView(this));
+        setContentView(R.layout.activity_main);
+        ViewGroup layout = findViewById(R.id.board);
+        d = new DrawView(this);
+        layout.addView(d);
+
+//        setContentView(new DrawView(this));
+
+        reset = findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                d.reset();
+            }
+        });
     }
 }
